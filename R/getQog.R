75b58a00c590661e769141e65dfce2b13b5fedbe
Markus Kainu <markuskainu@gmail.com>
2014-01-05 22:05:58 +0200

    Separate functions for extracting, reading and subsetting data
    Still need to think smarter cache, integration between functions and documentation, at least.

--- R/getQog.R
+++ R/getQog.R
@@ -13,21 +13,20 @@
 
 #' Retrieve Quality of Government Institute data
 #' 
-#' Download data from Quality of Government Institute Basic data at
+#' Download data from Quality of Government Institute data at
 #' \url{http://www.qog.pol.gu.se/data/}
 #'
-#' @param which.data A string. Specify the name of the QoG data set to retrieve. Currently available \code{"basic"}, \code{"standard"} or \code{"socialPolicy"}.
+#' @param which.data A string. Specify the name of the QoG data set to retrieve. Currently available \code{"basic"}, \code{"standard"} or \code{"social_policy"}.
 #' @param data.dir A string. Specify the path where to save the downloaded data file.
-
 #'  
 #' @return ??
 #' 
 #' @export
-#' @examples # dat <- getQog(data = "basic", country = c("Russia", "China"), indicator = c("undp_hdi", "fh_polity2"), year = 2000:2010)
+#' @examples # get_qog(which.data="basic",data.dir="data")
 #' @author Markus Kainu <markuskainu(at)gmail.com> 
 
-
-getQog <- function(which.data = "basic", data.dir = "data") {
+get_qog <- function(which.data = "basic", data.dir = "~/tmp", 
+                    cache=TRUE) {
     # URL's for different data's
     data.url <- "http://www.qogdata.pol.gu.se/data/"
     
@@ -56,7 +55,7 @@ getQog <- function(which.data = "basic", data.dir = "data") {
         download.file(data.url, destfile = local.csv)
     }
     
-    if (which.data == "socialPolicy") {
+    if (which.data == "social_policy") {
         # Remote csv that will be downloaded
         remote.csv <- "qog_soc_tsl_4apr12.csv"
         # Location and name of the csv file that will be saved on the local computer
@@ -68,3 +67,84 @@ getQog <- function(which.data = "basic", data.dir = "data") {
     }
 
 }
+
+#' Read Quality of Government Institute data in R
+#' 
+#' Download data from Quality of Government Institute Basic data at
+#' \url{http://www.qog.pol.gu.se/data/}
+#'
+#' @param which.data A string. Specify the name of the QoG data set to retrieve. Currently available \code{"basic"}, \code{"standard"} or \code{"social_policy"}.
+#' @param data.dir A string. Specify the path where to save the downloaded data file.
+#'  
+#' @return data.frame 
+#' 
+#' @export
+#' @examples # dat <- read_qog(which.data = "basic", data.dir="data")
+#' @author Markus Kainu <markuskainu(at)gmail.com> 
+
+read_qog <- function(which.data = "basic", data.dir = "~/tmp") {
+    if (which.data == "basic") {
+        # local csv that will be read
+        local.csv <- "qog_bas_ts_30aug13.csv"
+        # local data directory
+        local.path <- file.path(data.dir, local.csv)
+    }
+            
+    if (which.data == "standard") {
+        # local csv that will be read
+        local.csv <- "qog_std_ts_15may13.csv"
+        # local data directory
+        local.path <- file.path(data.dir, local.csv)
+    }
+    
+    if (which.data == "social_policy") {
+        # local csv that will be read
+        local.csv <- "qog_soc_tsl_4apr12.csv"
+        # local data directory
+        local.path <- file.path(data.dir, local.csv)
+    }
+    if (file.exists(local.path) == FALSE){
+        stop(paste(local.csv," file does not exist in ",
+                   data.dir, " folder. Make sure you have downloaded the file using get_qog() and check the directory.",sep=""))
+    }
+    # read the file
+    message(paste("Reading file from ",local.path, sep=""))
+    read.csv(local.path, sep=";")
+        
+}
+
+
+
+#' Subset Quality of Government Institute data in R
+#'
+#' @param country Character string. Name of the country. Value \code{"all"} returns data from all the countries.
+#' @param year Numeric. Year or years (eg. (\code{c("2000:2003")}). Value \code{"all"} returns data from all the years available.
+#' @param indicator Character string. Select indicator from the \code{indicator} column 
+#' of data frame returned by function \code{qogIndicator()}. More than one country or indicator 
+#' is allowed using \code{c("indicator1","indicator2")}
+
+
+subset_qog <- function(country=c("Finland","Sweden"),
+                       years=2000:2010,
+                       indicator=c("undp_hdi","fh_polity2")) {
+    if (exists("dat") == FALSE) {
+        stop("Object dat does not exist!")
+    }
+    # subset the countries
+    if (country != "all") df <- dat[dat$cname %in% country, ]
+    if (country == "all") df <- dat
+    # subset the variables
+    indicatorx <- c("cname","year",indicator)
+    df <- df[indicatorx]
+    # reshape the data into long format  
+    library(reshape2)
+    df.l <- melt(df, id.vars=c("cname","year"))
+    df.l <- df.l[!is.na(df.l$value), ]
+    names(df.l)[3] <- "variable"
+    # subset the years
+    df.l$year <- as.numeric(df.l$year)
+    if (years != "all") df.l <- df.l[df.l$year %in% years,]
+    if (years == "all") df.l <- df.l
+    df.l
+}
+
