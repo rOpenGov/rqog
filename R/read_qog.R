b9320eb9116e69b40e96b886e1178f6413847ae6
Markus Kainu <markuskainu@gmail.com>
2015-01-22 18:22:28 +0200

    data url updated with 2015 versions of data. Social Policy date replaced with new OECD data.

--- R/read_qog.R
+++ R/read_qog.R
@@ -15,7 +15,8 @@
 #' Download data from Quality of Government Institute Basic data at
 #' \url{http://www.qog.pol.gu.se/data/}
 #'
-#' @param which.data A string. Specify the name of the QoG data set to retrieve. Currently available \code{"basic"}, \code{"standard"} or \code{"social_policy"}.
+#' @param which.data A string. Specify the name of the QoG data set to retrieve. Currently available \code{"basic"}, \code{"standard"}, \code{"oecd"} or \code{"social_policy"}.
+#' @param data.type A string. Specify whether you want cross-sectional or time-series QoG data set to retrieve. Currently available \code{"cross-sectional"} or \code{"time-series"}.
 #' @param data.dir A string. Specify the path where to save the downloaded data file.
 #'  
 #' @return data.frame 
@@ -24,13 +25,29 @@
 #' @examples # dat <- read_qog(which.data = "basic", data.dir="data")
 #' @author Markus Kainu <markuskainu(at)gmail.com> 
 
-read_qog <- function(which.data = "basic", data.dir = "~/tmp") {
+read_qog <- function(which.data = "basic", data.type="time-series", data.dir = "~/tmp") {
         # Beginning of the URL's for all data's
         data.url.begin <- "http://www.qogdata.pol.gu.se/data/"
-        # local csv that will be read
-        if (which.data == "basic")          name.csv <- "qog_bas_ts_30aug13.csv"
-        if (which.data == "standard")       name.csv <- "qog_std_ts_15may13.csv"
-        if (which.data == "social_policy")  name.csv <- "qog_soc_tsl_4apr12.csv"        
+        
+        if (!(which.data %in% c("basic","standard","oecd"))) stop('Wrong data name, use "basic","standard" or "oecd" instead')
+        if (!(data.type %in% c("time-series","cross-sectional"))) stop('Wrong data name, use "time-series" or "cross-sectional" instead')
+        
+        # time-series data
+        if (data.type == "time-series") {
+            # local csv that will be read
+            if (which.data == "basic")          name.csv <- "qog_bas_ts_jan15.csv"
+            if (which.data == "standard")       name.csv <- "qog_std_ts_jan15.csv"
+            if (which.data == "oecd")           name.csv <- "qog_oecd_ts_jan15.csv"        
+        }
+        
+        # time-series data
+        if (data.type == "cross-sectional") {
+            # local csv that will be read
+            if (which.data == "basic")          name.csv <- "qog_bas_cs_jan15.csv"
+            if (which.data == "standard")       name.csv <- "qog_std_cs_jan15.csv"
+            if (which.data == "oecd")           name.csv <- "qog_oecd_cs_jan15.csv"
+        }
+        
         # creating local file path
         local.path <- file.path(data.dir, name.csv)
         # Check whether to local file exist.
@@ -40,17 +57,13 @@ read_qog <- function(which.data = "basic", data.dir = "~/tmp") {
             if (!file.exists(data.dir)) dir.create(data.dir)
             # Create the web address from where to fetch the csv
             data.url <- paste(data.url.begin, name.csv, sep = "")
-            message(paste("Local file not found.
-Downloading QoG ",which.data," data 
-from ",data.url, " 
-in file: ", local.path," 
------------------------------------
------------------------------------", sep=""))
+            message(paste("Local file not found. \n Downloading QoG",data.type,which.data,"data, \n",
+                              "from",data.url,"\n in file:", local.path,"\n", sep=" "))
             download.file(data.url, destfile = local.path)
      }
     # read the local file in every case
     message(paste("Reading local file from ",local.path, sep=""))
-    read.csv(local.path, sep=";")
+    read.csv(local.path, sep=",")
 }
 
 
