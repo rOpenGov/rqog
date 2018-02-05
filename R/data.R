be3d0a92af124517e3bb0971063cbe8b8010a108
Markus Kainu <markuskainu@gmail.com>
2018-02-05 14:27:11 +0200

    metadatas both for time-series and cross-sectional datas

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
 #' 
-#' Metadata for 2018 Quality of Government institute Basic Data
+#' Metadata for 2018 Quality of Government institute Basic Data - time-series
 #' 
 #' Generated automatically from spss-version of Quality of Government Basic data using \code{tidymetadata::create_metadata()} function. 
 #' Formatted in tidy-format with row per each value of an indicator. Numeric indicators have a single row per indicator.   
@@ -12,11 +12,26 @@
 #'    \item{label}{Value label for factor indicators}
 #'    \item{class}{Class of the indicator. Numeric, factor or character}
 #' }
-#' @Source \url{https://qog.pol.gu.se/data/datadownloads/qogbasicdata}
-"meta_basic"
+"meta_basic_ts"
+
+#' Metadata for 2018 Quality of Government institute Basic Data - cross-sectional
+#' 
+#' Generated automatically from spss-version of Quality of Government Basic data using \code{tidymetadata::create_metadata()} function. 
+#' Formatted in tidy-format with row per each value of an indicator. Numeric indicators have a single row per indicator.   
+#' 
+#' @format A data frame with 644 rows and five columns 
+#' \describe{
+#'    \item{code}{Indicator code in data}
+#'    \item{name}{Name of the indicator}
+#'    \item{value}{Indicator value for factor indicator}
+#'    \item{label}{Value label for factor indicators}
+#'    \item{class}{Class of the indicator. Numeric, factor or character}
+#' }
+"meta_basic_cs"
+
 
 #'
-#' Metadata for 2018 Quality of Government institute Standard Data
+#' Metadata for 2018 Quality of Government institute Standard Data - cross-sectional
 #' 
 #' Generated automatically from spss-version of Quality of Government Standard data using \code{tidymetadata::create_metadata()} function. 
 #' Formatted in tidy-format with row per each value of an indicator. Numeric indicators have a single row per indicator.   
@@ -29,11 +44,28 @@
 #'    \item{label}{Value label for factor indicators}
 #'    \item{class}{Class of the indicator. Numeric, factor or character}
 #' }
-#' @Source \url{https://qog.pol.gu.se/data/datadownloads/qogstandarddata}
-"meta_standard"
+"meta_standard_ts"
 
 #'
-#' Metadata for 2018 Quality of Government institute Standard Data
+#' Metadata for 2018 Quality of Government institute Standard Data - cross-sectional
+#' 
+#' Generated automatically from spss-version of Quality of Government Standard data using \code{tidymetadata::create_metadata()} function. 
+#' Formatted in tidy-format with row per each value of an indicator. Numeric indicators have a single row per indicator.   
+#' 
+#' @format A data frame with 2393 rows and five columns 
+#' \describe{
+#'    \item{code}{Indicator code in data}
+#'    \item{name}{Name of the indicator}
+#'    \item{value}{Indicator value for factor indicator}
+#'    \item{label}{Value label for factor indicators}
+#'    \item{class}{Class of the indicator. Numeric, factor or character}
+#' }
+"meta_standard_cs"
+
+
+
+#'
+#' Metadata for 2018 Quality of Government institute Standard Data - time-series
 #' 
 #' Generated automatically from spss-version of Quality of Government OECD data using \code{tidymetadata::create_metadata()} function. 
 #' Formatted in tidy-format with row per each value of an indicator. Numeric indicators have a single row per indicator.   
@@ -46,8 +78,24 @@
 #'    \item{label}{Value label for factor indicators}
 #'    \item{class}{Class of the indicator. Numeric, factor or character}
 #' }
-#' @Source \url{https://qog.pol.gu.se/data/datadownloads/qogoecddata}
-"meta_oecd"
+"meta_oecd_ts"
+
+#'
+#' Metadata for 2018 Quality of Government institute Standard Data - cross-sectional
+#' 
+#' Generated automatically from spss-version of Quality of Government OECD data using \code{tidymetadata::create_metadata()} function. 
+#' Formatted in tidy-format with row per each value of an indicator. Numeric indicators have a single row per indicator.   
+#' 
+#' @format A data frame with 1319 rows and five columns 
+#' \describe{
+#'    \item{code}{Indicator code in data}
+#'    \item{name}{Name of the indicator}
+#'    \item{value}{Indicator value for factor indicator}
+#'    \item{label}{Value label for factor indicators}
+#'    \item{class}{Class of the indicator. Numeric, factor or character}
+#' }
+"meta_oecd_cs"
+
 
 # # Create datas with
 # library(tidyverse)
@@ -55,19 +103,38 @@
 # library(rqog)
 # 
 # # Basic
+# ## time-series
 # d <- read_qog(which.data = "basic", data.type = "time-series", file.format = "sav")
-# meta_basic <- tidymetadata::create_metadata(d)
-# meta_basic <- meta_basic %>% select(code,name,value,label,class)
-# save(meta_basic, file="./data/meta_basic.rda")
+# meta_basic_ts <- tidymetadata::create_metadata(d) %>% 
+#   select(code,name,value,label,class)
+# save(meta_basic_ts, file="./data/meta_basic_ts.rda")
+# ## cross-section
+# d <- read_qog(which.data = "basic", data.type = "cross-sectional", file.format = "sav")
+# meta_basic_cs <- tidymetadata::create_metadata(d) %>% 
+#   select(code,name,value,label,class)
+# save(meta_basic_cs, file="./data/meta_basic_cs.rda")
 # 
 # # Standard
+# ## time-series
 # d <- read_qog(which.data = "standard", data.type = "time-series", file.format = "sav")
-# meta_standard <- tidymetadata::create_metadata(d)
-# meta_standard <- meta_standard %>% select(code,name,value,label,class)
-# save(meta_standard, file="./data/meta_standard.rda")
+# meta_standard_ts <- tidymetadata::create_metadata(d) %>% 
+#   select(code,name,value,label,class)
+# save(meta_standard_ts, file="./data/meta_standard_ts.rda")
+# ## cross-section
+# d <- read_qog(which.data = "standard", data.type = "cross-sectional", file.format = "sav")
+# meta_standard_cs <- tidymetadata::create_metadata(d) %>% 
+#   select(code,name,value,label,class)
+# save(meta_standard_cs, file="./data/meta_standard_cs.rda")
+# 
 # 
 # # Oecd
+# ## time-series
 # d <- read_qog(which.data = "oecd", data.type = "time-series", file.format = "sav")
-# meta_oecd <- tidymetadata::create_metadata(d)
-# meta_oecd <- meta_oecd %>% select(code,name,value,label,class)
-# save(meta_oecd, file="./data/meta_oecd.rda")
+# meta_oecd_ts <- tidymetadata::create_metadata(d) %>% 
+#   select(code,name,value,label,class)
+# save(meta_oecd_ts, file="./data/meta_oecd_ts.rda")
+# ## cross-section
+# d <- read_qog(which.data = "oecd", data.type = "cross-sectional", file.format = "sav")
+# meta_oecd_cs <- tidymetadata::create_metadata(d) %>% 
+#   select(code,name,value,label,class)
+# save(meta_oecd_cs, file="./data/meta_oecd_cs.rda")
