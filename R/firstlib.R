0a2220a95e550c5e0b980c84f418fbcc77979242
Markus Kainu <muuankarski@kapsi.fi>
2020-01-31 12:13:36 +0200

    metadatas and vignette updated

--- R/firstlib.R
+++ R/firstlib.R
@@ -2,4 +2,6 @@
 #' @importFrom haven read_dta
 #' @importFrom haven read_sav
 #' @importFrom readxl read_excel
+#' @importFrom utils download.file
+#' @importFrom utils read.csv
 .QogEnv <- new.env()
